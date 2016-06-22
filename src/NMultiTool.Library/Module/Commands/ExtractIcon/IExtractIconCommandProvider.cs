1125a4bb9a4bd321227e9dfb6e145f109d9efe2a
trondr <trondr@outlook.com>
2016-06-22 07:55:45 +0200

    Implemented extract icon command

---
+++ src/NMultiTool.Library/Module/Commands/ExtractIcon/IExtractIconCommandProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace NMultiTool.Library.Module.Commands.ExtractIcon
+{
+    public interface IExtractIconCommandProvider
+    {
+        int ExtratIcon(string sourceFile, string targetFolder);
+    }
+
+    public class ExtractIconCommandProvider : IExtractIconCommandProvider
+    {
+        public int ExtratIcon(string sourceFile, string targetFolder)
+        {
+            BitmapSource bmpSrc;
+            using (var sysicon = System.Drawing.Icon.ExtractAssociatedIcon(sourceFile))
+            {
+                bmpSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                            sysicon.Handle,
+                            System.Windows.Int32Rect.Empty,
+                            System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            }
+            var sourceFileInfo = new FileInfo(sourceFile);
+            var baseFileName =  Path.GetFileNameWithoutExtension(sourceFileInfo.Name);
+            var iconFile = Path.Combine(targetFolder, baseFileName + ".ico");
+            using (var fileStream = new FileStream(iconFile, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bmpSrc));
+                encoder.Save(fileStream);
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/NMultiTool/Module/Commands/ExtractIconCommand.cs
@@ -0,0 +1,28 @@
+﻿using NCmdLiner.Attributes;
+using NMultiTool.Library.Infrastructure;
+using NMultiTool.Library.Module.Commands.ExtractIcon;
+
+namespace NMultiTool.Module.Commands
+{
+    public class ExtractIconCommand : CommandDefinition
+    {
+        private readonly IExtractIconCommandProvider _extractIconCommandProvider;
+
+        public ExtractIconCommand(IExtractIconCommandProvider extractIconCommandProvider)
+        {
+            _extractIconCommandProvider = extractIconCommandProvider;
+        }
+
+        [Command(Description = "Extract icons from file.")]
+        public int ExtractIcon(
+            [RequiredCommandParameter(Description = "Binary file to extract icons from.",AlternativeName = "sf", ExampleValue = @"c:\windows\notepad.exe")]
+            string sourceFile, 
+            [RequiredCommandParameter(Description = "Save icon to this folder.",AlternativeName = "tf", ExampleValue = @"c:\temp")]
+            string targetFolder
+            )
+        {
+
+            return _extractIconCommandProvider.ExtratIcon(sourceFile, targetFolder);
+        }
+    }
+}
