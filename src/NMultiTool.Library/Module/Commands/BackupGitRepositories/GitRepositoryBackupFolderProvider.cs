4fa8af88e634e4c15840a6766f99337fa7258535
trondr <trondr@outlook.com>
2016-10-22 19:23:36 +0200

    Adding support for backing up git repositories from urls of type: file://...

--- src/NMultiTool.Library/Module/Commands/BackupGitRepositories/GitRepositoryBackupFolderProvider.cs
+++ src/NMultiTool.Library/Module/Commands/BackupGitRepositories/GitRepositoryBackupFolderProvider.cs
@@ -9,7 +9,7 @@ namespace NMultiTool.Library.Module.Commands.BackupGitRepositories
         public string GetRepositoryBackupFolder(string rootBackupFolder, string sourceUrl)
         {
             var sourceUri = new Uri(sourceUrl);
-            var sourceHost = GetSourceHost(sourceUri.Host);
+            var sourceHost = GetSourceHost(sourceUri);
             var sourceOwner = GetRepositoryOwner(sourceUri);
             var repositoryName = GetRepositoryName(sourceUri);
             var sourceRoot = string.Format("{0}-{1}", sourceHost, sourceOwner);
@@ -17,18 +17,77 @@ namespace NMultiTool.Library.Module.Commands.BackupGitRepositories
             return repositoryBackupFolder;
         }
 
-        private string GetSourceHost(string sourceUriHost)
+        private string GetSourceHost(Uri sourceUri)
         {
-            var sourceHost = sourceUriHost.Replace(".","-");
+            if (sourceUri == null) throw new ArgumentNullException(nameof(sourceUri));
+
+            string sourceHost;
+            if (sourceUri.Scheme == "file")
+            {
+                sourceHost = GetSourceHostFromFileUri(sourceUri);                
+            }
+            else
+            {
+                var sourceUriHost = sourceUri.Host;
+                sourceHost = sourceUriHost.Replace(".","-");
+            }            
+            return sourceHost;
+        }
+
+        private string GetSourceHostFromFileUri(Uri sourceUri)
+        {
+            string sourceHost;
+            if (sourceUri.IsUnc)
+            {
+                sourceHost = sourceUri.Host;                
+            }
+            else
+            {
+                var driveLetter = sourceUri.LocalPath[0].ToString();
+                sourceHost = driveLetter;    
+            }            
             return sourceHost;
         }
 
         private string GetRepositoryOwner(Uri sourceUri)
         {
+            string respositoryOwner;
+            if (sourceUri.Scheme == "file")
+            {
+                respositoryOwner = GetOwnerFromFileUri(sourceUri);             
+            }
+            else
+            {
+                respositoryOwner = GetOwnerFromWebUri(sourceUri);
+            }            
+            return respositoryOwner;
+        }
+
+        private string GetOwnerFromWebUri(Uri sourceUri)
+        {
             var absolutePath = sourceUri.AbsolutePath.TrimStart(new []{'/'});
             var pathParts = absolutePath.ToLower().Split('/');
             var allpathPartsButTheLast = pathParts.Take(pathParts.Length - 1);
             var respositoryOwner = string.Join("-", allpathPartsButTheLast);
+            return respositoryOwner;
+        }
+
+        private string GetOwnerFromFileUri(Uri sourceUri)
+        {
+            var absolutePath = sourceUri.AbsolutePath.TrimStart(new[] { '/' });
+            var pathParts = absolutePath.ToLower().Split('/');
+            var allpathPartsButTheLast = pathParts.Take(pathParts.Length - 1);
+            string respositoryOwner;
+            if (sourceUri.IsUnc)
+            {            
+                respositoryOwner = string.Join("-", allpathPartsButTheLast);
+            }
+            else
+            {
+                var allpathPartsButTheFirstAndTheLast = allpathPartsButTheLast.ToList();
+                allpathPartsButTheFirstAndTheLast.RemoveAt(0);    
+                respositoryOwner = string.Join("-", allpathPartsButTheFirstAndTheLast);
+            }
             return respositoryOwner;
         }
 
--- src/NMultiTool.Tests/Module/Commands/BackupGitRepositories/GitRepositoryBackupFolderProviderTests.cs
+++ src/NMultiTool.Tests/Module/Commands/BackupGitRepositories/GitRepositoryBackupFolderProviderTests.cs
@@ -11,6 +11,8 @@ namespace NMultiTool.Tests.Module.Commands.BackupGitRepositories
         [TestCase("https://someservices.kilnhg.com/Code/Repositories/Legacy/some-repository.git", @"c:\temp\rootBackupFolder", @"c:\temp\rootBackupFolder\someservices-kilnhg-com-code-repositories-legacy\some-repository")]
         [TestCase("https://someservices.kilnhg.com/Code/Repositories/Tools/some-repository.git", @"c:\temp\rootBackupFolder", @"c:\temp\rootBackupFolder\someservices-kilnhg-com-code-repositories-tools\some-repository")]  
         [TestCase("https://someuser@bitbucket.org/some-team/some-repository.git", @"c:\temp\rootBackupFolder", @"c:\temp\rootBackupFolder\bitbucket-org-some-team\some-repository")]
+        [TestCase("file://c:\\temp\\some-repository", @"c:\temp\rootBackupFolder", @"c:\temp\rootBackupFolder\c-temp\some-repository")]
+        [TestCase("file://\\\\someserver\\temp\\some-repository", @"c:\temp\rootBackupFolder", @"c:\temp\rootBackupFolder\someserver-temp\some-repository")]
         public void GetRepositoryBackupFolderTest(string sourceUrl, string rootBackupfolder, string expected)
         {
             var target = new GitRepositoryBackupFolderProvider();
