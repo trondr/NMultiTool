c5bf5920f4584fd280c76abcc40c0f5305536a1c
trondr <trondr@outlook.com>
2016-10-22 19:24:06 +0200

    Code cleanup

--- src/NMultiTool/Module/Commands/BackupGitRepositoriesCommandDefinition.cs
+++ src/NMultiTool/Module/Commands/BackupGitRepositoriesCommandDefinition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NCmdLiner.Attributes;
+﻿using NCmdLiner.Attributes;
 using NMultiTool.Library.Infrastructure;
 using NMultiTool.Library.Module.Commands.BackupGitRepositories;
 
